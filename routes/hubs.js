2a270ba04292eef26abee0247144c46dca47012c
fabianschu <48454910+fabianschu@users.noreply.github.com>
2022-04-23 12:01:07 +0200

    stores new hub in memory

--- routes/hubs.js
+++ routes/hubs.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
+const hubs = {};
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("hubs");
@@ -8,6 +10,8 @@ router.get("/", function (req, res, next) {
 
 /* GET users listing. */
 router.post("/", function (req, res, next) {
+  const { spaces, hubName } = req.body;
+  hubs[hubName] = spaces;
   res.send("hubs");
 });
 
